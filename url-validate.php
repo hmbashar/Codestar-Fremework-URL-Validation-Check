69faa9526cc0298b11b2c994ee44ea9b7fcf0784
Md Abul Bashar <hmbashar@users.noreply.github.com>
2018-09-24 14:14:44 +0600

    Update url-validate.php

--- url-validate.php
+++ url-validate.php
@@ -10,9 +10,12 @@
 if( ! function_exists( 'cs_validate_url' ) ) {
   function cs_validate_url( $value, $field ) {
 
-    if ( ! filter_var($value, FILTER_VALIDATE_URL) ) {
-      return esc_html__( 'Please write a valid url!', 'cs-framework' );
+    if (!empty($value)) {
+        if ( ! filter_var($value, FILTER_VALIDATE_URL) ) {
+            return esc_html__( 'Please write a valid url!', 'cs-framework' );
+          }
     }
+
 
   }
   add_filter( 'cs_validate_url', 'cs_validate_url', 10, 2 );
